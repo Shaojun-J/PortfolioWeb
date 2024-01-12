c96ed954543e618e76da558ecda339e305d05cff
Shaojun <68138142+Shaojun-J@users.noreply.github.com>
2024-01-12 16:28:01 -0500

    init server and client

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');  
+require('dotenv').config();
+
+
+const app = express();
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    // res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to the server of my portfolio!');
+});
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
+});
+
